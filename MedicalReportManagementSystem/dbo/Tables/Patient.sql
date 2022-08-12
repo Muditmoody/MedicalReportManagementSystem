2794d7958602510c2fffa906aed4cc98e51210a8
Mudit Aggarwal <Mudit Aggarwal@LAPTOP-HMF8Q5ET>
2022-08-12 13:43:02 -0400

    Add project files.

---
+++ MedicalReportManagementSystem/dbo/Tables/Patient.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Patient]
+(
+	[Patient_Id] INT NOT NULL PRIMARY KEY,
+    [Patient_Name] VARCHAR(100) NULL,
+    [Date_of_birth] DATETIME NULL,
+    [Gender] CHAR(1) NULL,
+    [Primary_Contact] VARCHAR(50) NULL,
+    [Secondary_Contact] VARCHAR(50) NULL
+)
